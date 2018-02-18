a931236207f542ec078c3e1ee90c8b868d774603
Marta Aguilar <martaguilar.soto@gmail.com>
2018-02-18 18:13:41 +0100

    coments

--- src/components/App.js
+++ src/components/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
 		})
 	}
 
+	//hace filtro y pinta
 	drawRepos() {
 		let allRepos = this.state.arrayRepos;
 		if(this.state.filterLanguage && this.state.filterText){
@@ -59,20 +60,22 @@ class App extends React.Component {
 
 	handleClick(event){
 		let valueText = event.target.value;//recogemos el valor introducido del input
-		let array = this.state.arrayRepos;
+		let array = this.state.arrayRepos;//guardamos el array
+		//filtra por nombre el array inicial y lo guarda en arrayFiltered
 		let arrayFiltered = array.filter(element => element.name.toLowerCase().includes(valueText.toLowerCase())
 	);
-
+		//compruebo si hay creado un filtro de lenguaje para si no filtrarlo con el array ya filtrado por texto
 		if (this.state.filterLanguage){
 			let languageSelected = this.state.filterLanguage;
 			let arraySuperFiltered = arrayFiltered.filter(element => element.language.toLowerCase().includes(languageSelected.toLowerCase())
 			);
-
+					//modifico estado con el valor del texto introducio y numero de elementos que tiene el array con los dos filtros
 					this.setState({
 						filterText : valueText,
 						counter: arraySuperFiltered.length
 					})
 		}
+		//si no modifico el estado del array filtrado por nombre
 		else {
 					this.setState({
 						filterText : valueText,
@@ -92,14 +95,14 @@ class App extends React.Component {
 		let textSelected = this.state.filterText;
 		let arraySuperFiltered1 = arrayFiltered1.filter(element => element.name.toLowerCase().includes(textSelected.toLowerCase())
 		);
-
+		//modifico estado con el valor del select y numero de elementos que tiene el array con los dos filtros
 		this.setState({
 			filterLanguage : theLanguage,
 			counter: arraySuperFiltered1.length
 		})
 
 	}
-
+	//si no modifico el estado del array filtrado por lenguaje
 	else{
 		this.setState({
 			filterLanguage : theLanguage,
@@ -116,7 +119,7 @@ class App extends React.Component {
 					<h1 className="title">Repos at Adalab in GitHub</h1>
 				</header>
 				<section className="container">
-					<div>{this.state.counter}</div>
+					<div className="counter">{this.state.counter}</div>
 					<Search whenWrite={this.handleClick} whenSelect={this.handleLanguage} />
 					{this.drawRepos()}
 				</section>
